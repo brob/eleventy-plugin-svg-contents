3f7d3eea297bdc16335aa51de46ef6a70d89e1b6
Bryan Robinson <bryanlrobinson@gmail.com>
2020-05-13 14:33:44 -0500

    Adds check for if not an svg

--- test/test.js
+++ test/test.js
@@ -6,9 +6,17 @@ const testSvg = Cheerio.load(`<svg xmlns="http://www.w3.org/2000/svg">
 <circle r="50"/>
 </svg>`)
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return an svg', function() {
+describe('SVG Test', function() {
+  describe('File contents received', function() {
+    it('Fails if not an SVG file', function() {
+      const getSVGContents = new GetSVGContents('/sample/notsvg.png');
+      const expected = Error('eleventy-plugin-svg-contents requires a filetype of svg');
+      assert.throws(
+        function() {getSVGContents.getSvg()},
+        expected
+      );
+    })
+    it('Returns SVG contents when provided SVG file', function() {
       const getSVGContents = new GetSVGContents('/sample/simple.svg');
       let svg = Cheerio.load(getSVGContents.getSvg())
       
